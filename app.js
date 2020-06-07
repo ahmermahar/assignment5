5b5a7dcb6885e11f70ed86a7b65f98556d1c7b28
ahmermahar <57357861+ahmermahar@users.noreply.github.com>
2020-06-07 17:44:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,301 @@
+// Assignment 1
+// Task 1
+//alert("Greetings user")
+
+// Task 2
+//alert("Error! Please Enter a valid password")
+
+
+//task 3
+//alert("Welcome to JS Land...\r\nHappy coding!")
+
+
+// task4
+//alert("Welcome to JS Land...")
+//alert("Happy coding!\r\n Prevent this page from creating additional dialogs")
+
+//task5
+// console.log("Hello... I can run js throgh my web browser's coonsole ") 
+
+
+
+
+//Assignment2
+//task1
+//var username;
+
+//task2
+//var myname;
+//myname="ahmer";
+
+//task3
+//var message;
+//message="Hello World!";
+//alert(message);
+
+//task4
+// var name="        ahmer";
+// var age="             21";
+// var cert="   certified developer";
+// alert(name);
+// alert(age);
+// alert(cert);
+
+//task5
+// var m="PIZZA\r\nPIZZ\r\nPIZ\r\nPI\r\nP"
+// alert(m)
+
+//task6
+// var email="ahmermahar82@gmail.com";
+// alert("My email address is "+email);
+
+//task7
+// var book="A smarter\r\nway to learn Javascript";
+// alert("I am trying to learn from the book "+book);
+
+//task8
+// document.write("Yah i can write html content through javascript")
+
+
+//Assigment3
+//task1
+// var age;
+// age=21;
+// alert(" I am "+age +" years old")
+
+//task2
+// var n=14;
+//alert("You have visited the website "+n+" times")
+
+//task3
+// var birthyear;
+// birthyear=1998;
+// document.write("My birth year is "+birthyear+"<br>Data type of my decleared variable is number");
+
+//task4
+// var visitor_name;
+// var product_name;
+// var quantity;
+// visitor_name="ahmer";
+// product_name="jeans";
+// quantity=10;
+// document.write(visitor_name+" ordered "+quantity+" "+product_name+" On abc clothing store");
+
+
+//Assignment4
+//task1
+//var a,b,c;
+
+//task2
+//illegal variable
+// 1- var 1x;
+// 2- var bo ok;
+//3- var alert;
+//4- var "x"
+//5- var 234y;
+//legal variable 
+//1- var myname;
+//2- var this_is_alert;
+//3- var _ahmm;
+//4- var ahm_112;
+//5- var Alert;
+
+//task3
+// var n="Rules for naming JS variables";
+// var s=n.bold();
+// document.write(s+"<br><br>")
+// document.write("Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable<br>Variables must begin with a ______, ______ or _____. For example $name, _name or name<br>Variable names are case _________<br> Variable names should not be JS _________")
+
+
+//Assignment5
+//task1
+// var num1=+prompt();
+// var num2=+prompt();
+// alert(num1+num2);
+
+//task2
+// var num1=+prompt();
+// var num2=+prompt();
+// alert(num1-num2);
+// alert(num1*num2);
+// alert(num1/num2);
+// alert(num1%num2);
+
+//task3
+// var num;
+// alert(num)
+// num=5;
+// num++;
+// alert(num)
+// num=num+7;
+// alert(num);
+// num--;
+// alert(num);
+// alert("the reminder is "+num%3);
+
+//task4
+// var price=600;
+// var tickets=5;
+// document.write("total cost to buy "+tickets+" tickets is "+price*tickets+" pkr")
+
+//task5
+// var x;
+// document.write("Table of 4 <br><br>")
+// for(x=1;x<=10;x++){
+//     document.write("4*"+x+"="+4*x+"<br>");
+    
+// }
+
+//task6
+// var c=25;
+// var f=(c*1.8)+32;
+// document.write(c+"C is "+f+"F<br>");
+// var f=70,
+// c=(f-32)*0.556;
+// document.write(f+"F is "+c+"C");
+
+//task7
+// document.write("Shopping Cart<br><br>".bold());
+// var item1=650,item2=100,quantity1=3,quantity2=7,sc=100;
+// document.write("Price of item 1 is "+item1+"<br>Quantity of item1 is "+quantity1);
+// document.write("<br>Price of item 2 is "+item2+"<br>Quantity of item2 is "+quantity2+"<br>Shipping Charges is "+sc);
+// document.write("<br>Total cost of your order is "+((item1*quantity1)+(item2*quantity2)+sc))
+
+//task8
+//var total_marks=900;
+//var obtained_marks=800;
+//document.write((obtained_marks/total_marks)*100);
+
+//task9
+//var usd-c=104.80;
+//var r-c=28;
+//var usd=10;
+//var r=25;
+//document.write("Total currencey in pkr is "+((usd-c*usd)+(r-c*r));
+
+//task10
+// var a=1;
+// document.write(((a+5)*10)/2);
+
+//task11
+//current_year=2020;
+//birth_year=1998;
+//document.write("Your age is "+(current_year-birth_year));
+
+//task12
+//var r=20;
+//var pie=3.142;
+//document.write("The radius of circle is "+r);
+//document.write("The circumference of circle is "+(2*pie*r));
+//document.write("The Area of circle is "+(pie*pie*r));
+
+//task13
+// var favourate_snake="chocalte";
+// var current_age=15;
+// var max_age=65;
+// var snake=3;
+// document.write("you will need "+((max_age-current_age)*3)+" chocalte chip till the ripe age of "+max_age)
+
+
+
+//Assigmment6
+//task1
+// var a=10;
+//++a will give 11 as result pre increment
+//a++ post increment 11 will be value of this so after this new value be 12
+//--a will give 11 as result pre decrement
+//a-- post decrement 11 will be value of this so after this new value be 10
+
+//task2
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// alert(result)
+// --a;               1
+//  --a - --b;           1
+//  --a - --b + ++b;     2
+//   --a - --b + ++b + b--;3
+
+//task3
+//var n=prompt();
+//alert("Greetings "+n)
+
+//task5
+// var x;
+//var n=+prompt();
+//if (n==0){
+//  n=5;
+//}
+// for(x=1;x<=10;x++){
+//     document.write(n+"*"+x+"="+n*x+"<br>)}
+
+//task6
+// var sub1,sub2,sub3,ob1,ob2,ob3;
+// var total=100;
+// alert("Enter subjects name");
+// sub1=prompt();
+// sub2=prompt();
+// sub3=prompt();
+// alert("Enter marks:");
+// ob1=+prompt();
+// ob2=+prompt();
+// ob3=+prompt();
+// document.write("Subjects Total Marks Obtained Marks Percentage<br>".bold());
+// document.write(sub1+" "+total+"\t    "+ob1+"         "+(ob1/total*100));
+// document.write("<br>"+sub2+" "+total+"    "+ob2+"         "+(ob2/total*100));
+// document.write("<br>"+sub3+" "+total+"    "+ob3+"         "+(ob3/total*100));
+// document.write("<br>      "+300+"    "+(ob1+ob2+ob3)+"         "+((ob1+ob2+ob3)/3));
+
+
+
+//Assignment12-13
+//task1
+// var n=prompt();
+// if(isNaN(n)){
+//     if (isUpperCaseAt(n)){
+//         document.write(n + " is a uppercase <br/>");
+//     }
+//     else{
+//             document.write(n + " is a lowercase <br/>"); 
+//         }
+     
+// }
+// else{
+// 	document.write(n + " is a number <br/>");
+//     }
+
+//task2
+// var n1=+prompt();
+// var n2=+prompt();
+// if (n1>n2){
+//     document.write(n1+"is larger");
+// }
+// else if (n1<n2){
+//     document.write(n2+"is larger");
+// }
+
+// else {
+//     document.write("equal")
+// }
+
+//task3
+// var n1=+prompt();
+// if (n1>0){
+//     document.write(n1+"is positive");
+// }
+// else if (n1<0){
+//     document.write(n2+"is negative");
+// }
+
+// else {
+//     document.write("zero")
+// }
+
+//task4
+// var ch=prompt();
+// if (ch=="A"||ch=="E"||ch=="I"||ch=="U"||ch=="S"||ch=="a"||ch=="e"||ch=="i"||ch=="u"||ch=="s"){
+//     alert("true");
+// }
+// else{
+//     alert("false")
+// }
+
